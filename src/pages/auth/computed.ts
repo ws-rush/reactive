be9c8a1a92eeaf67539e3a16402562f4d8f65242
wusaby-rush <wusaby.rush@zohomail.com>
2023-12-02 11:59:54 +0300

    Enhance computed property (with ts errors)

---
+++ src/pages/auth/computed.ts
@@ -0,0 +1,122 @@
+import { StateCreator, StoreApi, StoreMutatorIdentifier, Mutate } from "zustand"
+import { shallow } from "zustand/shallow"
+
+export type ComputedStateOpts<T> = {
+  keys?: (keyof T)[]
+  disableProxy?: boolean
+  equalityFn?: <Y>(a: Y, b: Y) => boolean
+}
+
+export type ComputedStateCreator = <
+  T extends object,
+  A extends object,
+  Mps extends [StoreMutatorIdentifier, unknown][] = [],
+  Mcs extends [StoreMutatorIdentifier, unknown][] = [],
+>(
+  f: StateCreator<T, [...Mps, ["chrisvander/zustand-computed", A]], Mcs>,
+//   compute: (state: T) => A,
+  opts?: ComputedStateOpts<T>,
+) => StateCreator<T, Mps, [["chrisvander/zustand-computed", A], ...Mcs], T & A>
+
+type Cast<T, U> = T extends U ? T : U
+type Write<T, U> = Omit<T, keyof U> & U
+type StoreCompute<S, A> = S extends {
+  getState: () => infer T
+}
+  ? Omit<StoreApi<T & A>, "setState">
+  : never
+type WithCompute<S, A> = Write<S, StoreCompute<S, A>>
+
+declare module "zustand" {
+  interface StoreMutators<S, A> {
+    "chrisvander/zustand-computed": WithCompute<Cast<S, object>, A>
+  }
+}
+
+type ComputedStateImpl = <T extends object, A extends object>(
+  f: StateCreator<T & { computed?: (state: T) => A }, [], []>,
+//   compute: (state: T) => A,
+  opts?: ComputedStateOpts<T>,
+) => StateCreator<T, [], [], T & A>
+
+type SetStateWithArgs = Parameters<ReturnType<ComputedStateImpl>>[0] extends (...args: infer U) => void
+  ? (...args: [...U, ...unknown[]]) => void
+  : never
+
+// function isComputeDefiened<T, A>(value: unknown): value is (state: T) => A {
+//   return typeof value === "function"
+// }
+
+const computedImpl: ComputedStateImpl = (f, opts) => {
+  // set of keys that have been accessed in any compute call
+  const trackedSelectors = new Set<string | number | symbol>()
+
+  return (set, get, api) => {
+    const compute = f(set, get, api)?.computed
+    if (!compute) {
+      return f(set, get, api)
+    }
+
+    type T = ReturnType<typeof f>
+    type A = ReturnType<typeof compute>
+
+    const equalityFn = opts?.equalityFn ?? shallow
+
+    if (opts?.keys) {
+      const selectorKeys = opts.keys
+      selectorKeys.forEach((key) => trackedSelectors.add(key))
+    }
+
+    // we track which selectors are accessed
+    const useSelectors = opts?.disableProxy !== true || !!opts?.keys
+    const useProxy = opts?.disableProxy !== true && !opts?.keys
+    const computeAndMerge = (state: T | (T & A)): T & A => {
+      // create a Proxy to track which selectors are accessed
+      const stateProxy = new Proxy(
+        { ...state },
+        {
+          get: (_, prop) => {
+            trackedSelectors.add(prop)
+            return state[prop as keyof T]
+          },
+        },
+      )
+
+      // calculate the new computed state
+      const computedState: A = compute(useProxy ? stateProxy : { ...state })
+
+      // if part of the computed state did not change according to the equalityFn
+      // then we use the object ref from the previous state. This is to prevent
+      // unnecessary re-renders.
+      for (const k of Object.keys(computedState) as (keyof A)[]) {
+        if (equalityFn(computedState[k], (state as T & A)[k])) {
+          computedState[k] = (state as T & A)[k]
+        }
+      }
+
+      return { ...state, ...computedState }
+    }
+
+    // higher level function to handle compute & compare overhead
+    const setWithComputed = (update: T | ((state: T) => T), replace?: boolean, ...args: unknown[]) => {
+      (set as SetStateWithArgs)((state: T): T & A => {
+        const updated = typeof update === "object" ? update : update(state)
+
+        if (useSelectors && trackedSelectors.size !== 0 && !Object.keys(updated).some((k) => trackedSelectors.has(k))) {
+          // if we have a selector set, but none of the updated keys are in the selector set, then we can skip the compute
+          return { ...state, ...updated } as T & A
+        }
+
+        return computeAndMerge({ ...state, ...updated })
+      }, replace, ...args)
+    }
+
+    const _api = api as Mutate<StoreApi<T>, [["chrisvander/zustand-computed", A]]>
+    _api.setState = setWithComputed
+    const st = f(setWithComputed, get, _api) as T & A
+    return Object.assign({}, st, compute(st))
+  }
+}
+
+export const extra = computedImpl as unknown as ComputedStateCreator
+export default extra
--- src/pages/auth/forgot-pass.tsx
+++ src/pages/auth/forgot-pass.tsx
@@ -1,5 +1,36 @@
 import { Trans } from "@lingui/macro";
+import { createStore, useStore } from "zustand";
+import { extra } from "./computed";
+
+const store = createStore(extra((set) => ({
+  count: 0,
+  actions: {
+    inc() {
+      set((state) => ({ count: state.count + 1 }))
+    }
+  },
+  computed: (state) => ({
+    get doubled() {
+      return state.count * 2
+    },
+    twice: state.count * 2
+  }),
+  // TODO: add queries
+  // queries: {
+  //   users: async () => {
+  //     return {
+  //       name: 'chris'
+  //     }
+  //   }
+  // }
+})
+))
+
+const { inc } = store.getState().actions
 
 export default function Component() {
-  return <h1><Trans>forgot password</Trans></h1>
+  const double = useStore(store, (state) => state.doubled)
+  return <h1><Trans>forgot password, {double}</Trans>
+  <button type="button" onClick={inc}>inc</button>
+  </h1>
 }
