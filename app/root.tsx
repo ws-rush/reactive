2c798476a87465f79b4b0f5094122733047e2b86
rush <rush@wusaby.com>
2024-06-06 16:41:32 +0300

    feat: add pages error handler

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,6 @@
+import { Trans } from '@lingui/macro'
 import { Helmet } from 'react-helmet'
+import { useRouteError } from 'react-router-dom'
 
 export function Component() {
   return (
@@ -21,5 +23,41 @@ export function Component() {
       </Helmet>
       <Outlet />
     </>
+  )
+}
+
+export function ErrorBoundray() {
+  const navigate = useNavigate()
+  const error = useRouteError()
+
+  // log error in sentry or other log service
+  // eslint-disable-next-line no-console
+  console.log(error)
+
+  function logout() {
+    // also remove tokens
+    navigate('/login')
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1>
+        <Trans>Sorry, there is an error.</Trans>
+      </h1>
+      <div className="flex gap-3">
+        <button
+          onClick={() => navigate('/')}
+          type="button"
+        >
+          <Trans>Go to Home</Trans>
+        </button>
+        <button
+          onClick={() => logout()}
+          type="button"
+        >
+          <Trans>Logout</Trans>
+        </button>
+      </div>
+    </div>
   )
 }
