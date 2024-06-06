0605fd9e1bb97fb0e89573b435201ba188375dcd
rush <rush@wusaby.com>
2024-06-06 15:17:42 +0300

    feat: export page loader

--- app/main.tsx
+++ app/main.tsx
@@ -9,7 +9,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
-function Loader() {
+export function Loader() {
   return (
     <div className="h-screen grid place-items-center">
       <h1>I am Loader, Put your Logo here</h1>
