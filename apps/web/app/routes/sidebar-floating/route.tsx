454802f4754b2ce58a0664923d0d33594b6f70b0
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2025-12-14 21:52:10 +0300

    feat: add new `sidebar-floating` route with app sidebar and breadcrumb navigation.

--- apps/web/app/routes/sidebar-floating/route.tsx
+++ apps/web/app/routes/sidebar-floating/route.tsx
@@ -19,9 +19,12 @@ export default function Page() {
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
                     <SidebarTrigger className="-ml-1" />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Separator
+                        orientation="vertical"
+                        className="mr-2 data-[orientation=vertical]:h-4"
+                    />
                     <Breadcrumb>
                         <BreadcrumbList>
                             <BreadcrumbItem className="hidden md:block">
@@ -37,12 +40,12 @@ export default function Page() {
                     </Breadcrumb>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4">
-                    {Array.from({ length: 24 }).map((_, index) => (
-                        <div
-                            key={index}
-                            className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
-                        />
-                    ))}
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                        <div className="bg-muted/50 aspect-video rounded-xl" />
+                        <div className="bg-muted/50 aspect-video rounded-xl" />
+                        <div className="bg-muted/50 aspect-video rounded-xl" />
+                    </div>
+                    <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
                 </div>
             </SidebarInset>
         </SidebarProvider>
